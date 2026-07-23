b88fff9f4674a1b8d9f04dc94adc414ed6178e04
guillaut5@gmail.com <guillauT5@gmail.com>
2026-07-23 10:21:33 +0200

    injetion dela version automatique

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import { readFileSync } from 'fs'
 import { defineConfig } from 'vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
+const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url)))
+
 export default defineConfig({
   plugins: [viteSingleFile()],
+  define: {
+    __APP_VERSION__: JSON.stringify(pkg.version),
+  },
   build: {
     outDir: 'dist',
     assetsInlineLimit: Infinity,
